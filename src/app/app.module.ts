2a50a38c559874f89a6a3096595b8426748677b1
Diomar Majano <http.diomar@gmail.com>
2025-11-24 13:02:29 -0300

    create:(Componentes) creacion de los componentes que conforman la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { BalanceComponent } from './balance/balance.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import { PasswordComponent } from './password/password.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterServiceComponent } from './register-service/register-service.component';
+import { ServicesComponent } from './services/services.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InicioComponent,
+    BalanceComponent,
+    LoginComponent,
+    NavComponent,
+    PasswordComponent,
+    PerfilComponent,
+    RegisterComponent,
+    RegisterServiceComponent,
+    ServicesComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
